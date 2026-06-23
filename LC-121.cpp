77632868f0fbfe98b242e6b302a4feac80f8275f
Durgesh Gupta <durgeshg659@gmail.com>
2026-06-23 12:03:33 +0530

    Added file

--- LC-121.cpp
+++ LC-121.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minbuy=INT_MAX;
-        int maxPro=0;
 
-        for(int i=0;i<prices.size();i++){
-            if(prices[i]<minbuy){
-                minbuy=prices[i];
-            }
-            int profit = prices[i]-minbuy;
+        int minBuy = INT_MAX;
+        int maxPro = 0;
 
-            maxPro=max(maxPro, profit);
+        for(int i = 0; i < prices.size(); i++) {
+
+            // Calculate profit if sold today
+            maxPro = max(maxPro, prices[i] - minBuy);
+
+            // Update minimum buying price
+            minBuy = min(minBuy, prices[i]);
         }
+
         return maxPro;
-        
     }
 };
